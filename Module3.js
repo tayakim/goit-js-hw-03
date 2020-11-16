de4da90e208c21643568fffbd9cb3b0250e6bc8f
Tatiana <taulru@yandex.com>
2020-11-16 01:25:05 +0100

    HW3

--- Module3.js
+++ Module3.js
@@ -1,45 +1,123 @@
 // Task #1
-// const getItemsString = function(array) {
-//     'use strict';
-//     let result = "";
-
-//     for (let i = 0; i < array.length; i += 1) {
-//         result += (`${i + 1} - ${array[i]}\n`)
-//     }
-
-//     return result;
+// const user = {
+//     name: 'Mango',
+//     age: 20,
+//     hobby: 'html',
+//     premium: true,
 // };
+// // add new 
+// user.mood = 'happy';
+// // change 
+// user.hobby = 'skydiving';
+// user.premium = false;
 
-//console.log(getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
-/*
-'1 - Mango
-2 - Poly
-3 - Ajax
-4 - Lux
-5 - Jay
-6 - Kong
-'
-*/
-
-// console.log(getItemsString([5, 10, 15]));
-/*
-'1 - 5
-2 - 10
-3 - 15
-'
-*/
+// for (const person in user) {
+//     Object.keys(user);
+// }
+// console.log(user);
 
 // Task #2
+// const countProps = function(obj) {
+//     let count = 0;
+//     for (const key in obj) {
+//         count += 1;
+//     }
+//     return count;
+// };
 
-const calculateEngravingPrice = (message = "", pricePerWord = 0) => pricePerWord * message.split(' ').length;
+// console.log(countProps({})); // 0
+
+// console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+// console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
+
+// Task #3
+
+// const findBestEmployee = function(employees) {
+//     let max = 0;
+//     let name = "";
+//     const keys = Object.keys(employees);
+//     for (const key of keys) {
+//         if (max < employees[key]) {
+//             max = employees[key];
+//             name = key;
+//         }
+//     }
+//     return name;
+// };
 
 
-console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10)); // 80
+// console.log(
+//     findBestEmployee({
+//         ann: 29,
+//         david: 35,
+//         helen: 1,
+//         lorence: 99,
+//     }),
+// ); // lorence
 
-//console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 20)); // 160
+// console.log(
+//     findBestEmployee({
+//         poly: 12,
+//         mango: 17,
+//         ajax: 4,
+//     }),
+// ); // mango
 
-//console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40)); // 240
+// console.log(
+//     findBestEmployee({
+//         lux: 147,
+//         david: 21,
+//         kiwi: 19,
+//         chelsy: 38,
+//     }),
+// ); // lux
 
-//console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20)); // 120
+// Task #4
+// const countTotalSalary = function(employees) {
+//     let sum = 0;
+//     for (let salary of Object.values(employees)) {
+//         sum += salary;
+//     }
+//     return sum;
 
-//console.log(calculateEngravingPrice('Uno', 100)); // 100
+// };
+
+// console.log(countTotalSalary({})); // 0
+// console.log(
+//     countTotalSalary({
+//         mango: 100,
+//         poly: 150,
+//         alfred: 80,
+//     }),
+// ); // 330
+// console.log(
+//     countTotalSalary({
+//         kiwi: 200,
+//         lux: 50,
+//         chelsy: 150,
+//     }),
+// ); // 400
+
+// Task #5
+// const products = [
+//     { name: 'Радар', price: 1300, quantity: 4 },
+//     { name: 'Сканер', price: 2700, quantity: 3 },
+//     { name: 'Дроид', price: 400, quantity: 7 },
+//     { name: 'Захват', price: 1200, quantity: 2 },
+// ];
+
+// const getAllPropValues = function(arr, prop) {
+//     const result = [];
+//     for (product of arr) {
+//         result.push(product[prop]);
+//     }
+//     console.log(result);
+// };
+
+
+// console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+
+// console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+
+// console.log(getAllPropValues(products, 'category')); // []
